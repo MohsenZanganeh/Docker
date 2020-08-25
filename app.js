a9c92114f9b2d653583f82773b9235db3bfcd99a
MYAGAH\M.Zangane <mohsen.1398.pg@gmail.com>
2020-08-25 12:52:49 +0430

    network in docker compose

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 let express = require("express")
+let mongo   = require("mongo")
+const { MongoClient } = require("mongodb")
+let db=new MongoClient("mongodb://mongo:27017/testdb")
 require("dotenv").config()
+
 let app = express()
-app.get("/", (req, res) => {
+
+
+app.get("/",async (req, res) => {
+    await db.connect();
+
+    // Establish and verify connection
+    await db.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
     res .send("Hello World")
 })
+
 app.listen(process.env.PORT, () => {
     console.log("Connect To server....",process.env.PORT)
 }) 
